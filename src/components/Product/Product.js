8e42ee87424b2495b9ef73f7657dc760fde085be
GHSoyad <ghsoyad@gmail.com>
2022-10-09 02:33:31 +0600

    Added product cards

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const { name } = props.product;
+
+    const { name, img, price, ratings } = props.product;
     return (
         <div className='product'>
-            <p>{name}</p>
+            <div className='product-info'>
+                <img src={img} alt="" onError={e => {
+                    e.currentTarget.onerror = null;
+                    e.currentTarget.src = 'images/no-image.png'
+                }} />
+                <p className='bold primary product-name'>{name}</p>
+            </div>
+            <div>
+                <div className='product-price'>
+                    <p><span className='bold'>Price: $</span>{price}</p>
+                    <p><span className='bold'>Rating: </span>{ratings}</p>
+                </div>
+                <button>Add to Cart</button>
+            </div>
         </div>
     );
 };
